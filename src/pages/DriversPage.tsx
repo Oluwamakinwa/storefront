e3c6088cb8e870c7c965ed1b33b6628c03df0947
Ryan Rowe <ryanrrowe@gmail.com>
2020-06-15 14:23:02 -0700

    updated css for the embed page, but not liking it yet

--- src/pages/DriversPage.tsx
+++ src/pages/DriversPage.tsx
@@ -1,5 +1,6 @@
 import { IAppState } from '../store/app';
 import { Typography } from '@material-ui/core';
+import { useContent } from '../store/cms';
 import { useIsSmall } from '../common/hooks';
 import { useSelector } from 'react-redux';
 import BaseLayout from '../layouts/BaseLayout';
@@ -11,10 +12,11 @@ import styles from './DriversPage.module.scss';
 const DriversPage: React.FC = () => {
   const isSmall = useIsSmall();
   const driverFormId = useSelector<IAppState, string>((state) => state.cms.config.driverFormId);
+  const pageTitle = useContent('drive_page_title');
   const [loading, setLoading] = useState<boolean>(true);
 
   return (
-    <BaseLayout padding={0} maxWidth="unset">
+    <BaseLayout padding={pageTitle ? undefined : 0} title={pageTitle} maxWidth={pageTitle ? undefined : 'unset'}>
       <div className={classNames(styles.container, { [styles.small]: isSmall, [styles.loading]: loading })}>
         <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
         {driverFormId && (
@@ -22,10 +24,10 @@ const DriversPage: React.FC = () => {
             onLoad={() => setLoading(false)}
             title="Driver Application"
             className="airtable-embed airtable-dynamic-height"
-            src={`https://airtable.com/embed/${driverFormId}?backgroundColor=white`}
+            src={`https://airtable.com/embed/${driverFormId}?backgroundColor=white&layout=card`}
             frameBorder="0"
             width="100%"
-            height={isSmall ? 2400 : 2280}
+            height={pageTitle ? 800 : isSmall ? 2400 : 2280}
             style={{ backgroundColor: 'hsl(0,0%,98%)' }}
           />
         )}
